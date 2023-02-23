c7b2df5195650add43ff6d01a9678acdf30f7c68
BuildTools <altamiranolucas2013@gmail.com>
2023-02-23 19:26:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { Client, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
+const http = require('http');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -17,5 +18,18 @@ for (const file of eventFiles) {
 		client.on(event.name, (...args) => event.execute(...args));
 	}
 }
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Bot Prendido!!!!!!');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
 
 client.login(token);
